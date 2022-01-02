ef7a1ffa088a5ada79aa83cd0d0068ba8672381a
sungwoo choi <sqlstyle@naver.com>
2022-01-02 22:25:23 +0900

    16 to 10

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import multiprocessing as mp
+from multiprocessing import Process
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -11,8 +12,19 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    workspace = ['zzz','11'];
-    print(workspace[0])  # Press Ctrl+F8 to toggle the breakpoint.
-    #print_hi(workspace)
+    #모든 워크스페이스를 불러옴.
+    #근데 어짜피 한디비에서 긁어올때 순서대로 가져오지 않나?
+
+    workspace = ['a12bdcaed','c12bd3aed','d12dd32ed'];
+    data = []
+    #16진수인 워크스페이스 DB들을 10진수로 변환한 후 순서대로 나열한다.
+    for i in workspace:
+        data.append(int(i,16))
+
+    print(sorted(data))
+
+    p = Process(target=print_hi, args=('bob',))
+    p.start()
+    p.join()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
